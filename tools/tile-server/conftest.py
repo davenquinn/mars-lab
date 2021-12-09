7093130442db91d40b4104abd19d9ee1e73bfbdc
Daven Quinn <dev@davenquinn.com>
2021-12-09 15:33:04 -0600

    Replace tile server code with submodule

--- tools/tile-server/conftest.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-disable_loggers = ["rasterio"]
-
-
-def pytest_configure():
-    for logger_name in disable_loggers:
-        logger = logging.getLogger(logger_name)
-        logger.disabled = True
